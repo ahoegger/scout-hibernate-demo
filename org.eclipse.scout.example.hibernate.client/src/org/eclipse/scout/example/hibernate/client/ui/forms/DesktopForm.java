291b3e8c374bda6e08cf3949464768c993869f8b
Andreas Hoegger <github@ahoegger.ch>
2012-07-16 22:18:28 +0200

    initial commit

---
+++ org.eclipse.scout.example.hibernate.client/src/org/eclipse/scout/example/hibernate/client/ui/forms/DesktopForm.java
@@ -0,0 +1,196 @@
+package org.eclipse.scout.example.hibernate.client.ui.forms;
+
+import org.eclipse.scout.commons.annotations.ConfigOperation;
+import org.eclipse.scout.commons.annotations.FormData;
+import org.eclipse.scout.commons.annotations.FormData.SdkCommand;
+import org.eclipse.scout.commons.annotations.Order;
+import org.eclipse.scout.commons.exception.ProcessingException;
+import org.eclipse.scout.example.hibernate.client.ui.forms.DesktopForm.MainBox.PersonTableField;
+import org.eclipse.scout.example.hibernate.shared.services.process.DesktopFormData;
+import org.eclipse.scout.example.hibernate.shared.services.process.IDesktopProcessService;
+import org.eclipse.scout.rt.client.ui.action.menu.AbstractMenu;
+import org.eclipse.scout.rt.client.ui.basic.table.AbstractTable;
+import org.eclipse.scout.rt.client.ui.basic.table.columns.AbstractLongColumn;
+import org.eclipse.scout.rt.client.ui.basic.table.columns.AbstractStringColumn;
+import org.eclipse.scout.rt.client.ui.form.AbstractForm;
+import org.eclipse.scout.rt.client.ui.form.AbstractFormHandler;
+import org.eclipse.scout.rt.client.ui.form.fields.groupbox.AbstractGroupBox;
+import org.eclipse.scout.rt.client.ui.form.fields.tablefield.AbstractTableField;
+import org.eclipse.scout.rt.shared.TEXTS;
+import org.eclipse.scout.service.SERVICES;
+
+@FormData(value = DesktopFormData.class, sdkCommand = SdkCommand.CREATE)
+public class DesktopForm extends AbstractForm {
+
+  public DesktopForm() throws ProcessingException {
+    super();
+  }
+
+  @Override
+  protected boolean getConfiguredAskIfNeedSave() {
+    return false;
+  }
+
+  @Override
+  protected int getConfiguredDisplayHint() {
+    return DISPLAY_HINT_VIEW;
+  }
+
+  @Override
+  protected String getConfiguredDisplayViewId() {
+    return VIEW_ID_CENTER;
+  }
+
+  @Override
+  protected String getConfiguredIconId() {
+    return org.eclipse.scout.example.hibernate.shared.Icons.EclipseScout;
+  }
+
+  public MainBox getMainBox() {
+    return getFieldByClass(MainBox.class);
+  }
+
+  public PersonTableField getPersonTableField() {
+    return getFieldByClass(PersonTableField.class);
+  }
+
+  @Order(10.0)
+  public class MainBox extends AbstractGroupBox {
+
+    @Order(10.0)
+    public class PersonTableField extends AbstractTableField<PersonTableField.Table> {
+
+      @Override
+      protected boolean getConfiguredLabelVisible() {
+        return false;
+      }
+
+      @Order(10.0)
+      public class Table extends AbstractTable {
+
+        public NameColumn getNameColumn() {
+          return getColumnSet().getColumnByClass(NameColumn.class);
+        }
+
+        public PrenameColumn getPrenameColumn() {
+          return getColumnSet().getColumnByClass(PrenameColumn.class);
+        }
+
+        public IdColumn getIdColumn() {
+          return getColumnSet().getColumnByClass(IdColumn.class);
+        }
+
+        @Order(10.0)
+        public class IdColumn extends AbstractLongColumn {
+
+          @Override
+          protected boolean getConfiguredDisplayable() {
+            return false;
+          }
+
+          @Override
+          protected boolean getConfiguredVisible() {
+            return false;
+          }
+        }
+
+        @Order(20.0)
+        public class NameColumn extends AbstractStringColumn {
+
+          @Override
+          protected String getConfiguredHeaderText() {
+            return TEXTS.get("Name");
+          }
+        }
+
+        @Order(30.0)
+        public class PrenameColumn extends AbstractStringColumn {
+
+          @Override
+          protected String getConfiguredHeaderText() {
+            return TEXTS.get("Prename");
+          }
+        }
+
+        @Order(10.0)
+        public class EditMenu extends AbstractMenu {
+
+          @Override
+          protected String getConfiguredText() {
+            return TEXTS.get("EditFilterMenu");
+          }
+
+          @Override
+          protected void execAction() throws ProcessingException {
+            PersonForm form = new PersonForm();
+            Long personId = getIdColumn().getSelectedValue();
+            if (personId != null) {
+              form.setPersonNr(personId);
+              form.startModify();
+              form.waitFor();
+              if (form.isFormStored()) {
+                reloadPersonTable();
+              }
+            }
+
+          }
+
+        }
+
+        @Order(20.0)
+        public class NewMenu extends AbstractMenu {
+
+          @Override
+          protected String getConfiguredText() {
+            return TEXTS.get("New");
+          }
+
+          @Override
+          protected boolean getConfiguredEmptySpaceAction() {
+            return true;
+          }
+
+          @Override
+          protected void execAction() throws ProcessingException {
+            PersonForm form = new PersonForm();
+            form.startNew();
+            form.waitFor();
+            if (form.isFormStored()) {
+              reloadPersonTable();
+            }
+          }
+        }
+      }
+    }
+
+  }
+
+  /**
+   * @throws ProcessingException
+   */
+  private void reloadPersonTable() throws ProcessingException {
+    IDesktopProcessService service = SERVICES.getService(IDesktopProcessService.class);
+    DesktopFormData formData = new DesktopFormData();
+    exportFormData(formData);
+    formData = service.load(formData);
+    importFormData(formData);
+  }
+
+  public class ViewHandler extends AbstractFormHandler {
+
+    @Override
+    protected void execLoad() throws ProcessingException {
+      reloadPersonTable();
+
+    }
+  }
+
+  @Override
+  protected String getConfiguredTitle() {
+    return TEXTS.get("Persons");
+  }
+
+  public void startView() throws ProcessingException {
+    startInternal(new ViewHandler());
+  }
+}
---
+++ org.eclipse.scout.example.hibernate.client/src/org/eclipse/scout/example/hibernate/client/ui/forms/PersonForm.java
@@ -0,0 +1,160 @@
+package org.eclipse.scout.example.hibernate.client.ui.forms;
+
+import org.eclipse.scout.commons.annotations.FormData;
+import org.eclipse.scout.commons.annotations.FormData.SdkCommand;
+import org.eclipse.scout.commons.annotations.Order;
+import org.eclipse.scout.commons.exception.ProcessingException;
+import org.eclipse.scout.example.hibernate.client.ui.forms.PersonForm.MainBox.CancelButton;
+import org.eclipse.scout.example.hibernate.client.ui.forms.PersonForm.MainBox.NameField;
+import org.eclipse.scout.example.hibernate.client.ui.forms.PersonForm.MainBox.OkButton;
+import org.eclipse.scout.example.hibernate.client.ui.forms.PersonForm.MainBox.PrenameField;
+import org.eclipse.scout.example.hibernate.client.ui.forms.PersonForm.MainBox.ThrowExceptionField;
+import org.eclipse.scout.example.hibernate.shared.security.UpdatePersonPermission;
+import org.eclipse.scout.example.hibernate.shared.services.process.IPersonProcessService;
+import org.eclipse.scout.example.hibernate.shared.services.process.PersonFormData;
+import org.eclipse.scout.rt.client.ui.form.AbstractForm;
+import org.eclipse.scout.rt.client.ui.form.AbstractFormHandler;
+import org.eclipse.scout.rt.client.ui.form.fields.booleanfield.AbstractBooleanField;
+import org.eclipse.scout.rt.client.ui.form.fields.button.AbstractCancelButton;
+import org.eclipse.scout.rt.client.ui.form.fields.button.AbstractOkButton;
+import org.eclipse.scout.rt.client.ui.form.fields.groupbox.AbstractGroupBox;
+import org.eclipse.scout.rt.client.ui.form.fields.stringfield.AbstractStringField;
+import org.eclipse.scout.rt.shared.TEXTS;
+import org.eclipse.scout.service.SERVICES;
+
+@FormData(value = PersonFormData.class, sdkCommand = SdkCommand.CREATE)
+public class PersonForm extends AbstractForm {
+
+  private Long personNr;
+
+  public PersonForm() throws ProcessingException {
+    super();
+  }
+
+  @Override
+  protected String getConfiguredTitle() {
+    return TEXTS.get("Person");
+  }
+
+  public CancelButton getCancelButton() {
+    return getFieldByClass(CancelButton.class);
+  }
+
+  @FormData
+  public Long getPersonNr() {
+    return personNr;
+  }
+
+  @FormData
+  public void setPersonNr(Long personNr) {
+    this.personNr = personNr;
+  }
+
+  public void startModify() throws ProcessingException {
+    startInternal(new ModifyHandler());
+  }
+
+  public void startNew() throws ProcessingException {
+    startInternal(new NewHandler());
+  }
+
+  public MainBox getMainBox() {
+    return getFieldByClass(MainBox.class);
+  }
+
+  public NameField getNameField() {
+    return getFieldByClass(NameField.class);
+  }
+
+  public OkButton getOkButton() {
+    return getFieldByClass(OkButton.class);
+  }
+
+  public PrenameField getPrenameField() {
+    return getFieldByClass(PrenameField.class);
+  }
+
+  public ThrowExceptionField getThrowExceptionField() {
+    return getFieldByClass(ThrowExceptionField.class);
+  }
+
+  @Order(10.0)
+  public class MainBox extends AbstractGroupBox {
+
+    @Order(10.0)
+    public class NameField extends AbstractStringField {
+
+      @Override
+      protected String getConfiguredLabel() {
+        return TEXTS.get("Name");
+      }
+    }
+
+    @Order(20.0)
+    public class PrenameField extends AbstractStringField {
+
+      @Override
+      protected String getConfiguredLabel() {
+        return TEXTS.get("Prename");
+      }
+    }
+
+    @Order(30.0)
+    public class ThrowExceptionField extends AbstractBooleanField {
+
+      @Override
+      protected String getConfiguredLabel() {
+        return TEXTS.get("ThrowException");
+      }
+    }
+
+    @Order(40.0)
+    public class OkButton extends AbstractOkButton {
+    }
+
+    @Order(50.0)
+    public class CancelButton extends AbstractCancelButton {
+    }
+  }
+
+  public class ModifyHandler extends AbstractFormHandler {
+
+    @Override
+    public void execLoad() throws ProcessingException {
+      IPersonProcessService service = SERVICES.getService(IPersonProcessService.class);
+      PersonFormData formData = new PersonFormData();
+      exportFormData(formData);
+      formData = service.load(formData);
+      importFormData(formData);
+      setEnabledPermission(new UpdatePersonPermission());
+    }
+
+    @Override
+    public void execStore() throws ProcessingException {
+      IPersonProcessService service = SERVICES.getService(IPersonProcessService.class);
+      PersonFormData formData = new PersonFormData();
+      exportFormData(formData);
+      formData = service.store(formData);
+    }
+  }
+
+  public class NewHandler extends AbstractFormHandler {
+
+    @Override
+    public void execLoad() throws ProcessingException {
+      IPersonProcessService service = SERVICES.getService(IPersonProcessService.class);
+      PersonFormData formData = new PersonFormData();
+      exportFormData(formData);
+      formData = service.prepareCreate(formData);
+      importFormData(formData);
+    }
+
+    @Override
+    public void execStore() throws ProcessingException {
+      IPersonProcessService service = SERVICES.getService(IPersonProcessService.class);
+      PersonFormData formData = new PersonFormData();
+      exportFormData(formData);
+      formData = service.create(formData);
+    }
+  }
+}
